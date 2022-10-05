5429798d21363e5af99f304fb830d9ba70c00d48
Becky449 <82038825+Becky449@users.noreply.github.com>
2022-10-05 15:52:11 +0300

    fix linter errors

--- modules/storage.js
+++ modules/storage.js
@@ -1,30 +1,30 @@
 export default class Storage {
-    static getBooks() {
-      let collection;
-      if (localStorage.getItem('collection') === null) {
-        collection = [];
-      } else {
-        collection = JSON.parse(localStorage.getItem('collection'));
-      }
-  
-      return collection;
+  static getBooks() {
+    let collection;
+    if (localStorage.getItem('collection') === null) {
+      collection = [];
+    } else {
+      collection = JSON.parse(localStorage.getItem('collection'));
     }
-  
-    static newBook(book) {
-      const collection = Storage.getBooks();
-      collection.push(book);
-      localStorage.setItem('collection', JSON.stringify(collection));
-    }
-  
-    static delBook(author) {
-      const collection = Storage.getBooks();
-  
-      collection.forEach((book, index) => {
-        if (book.author === author) {
-          collection.splice(index, 1);
-        }
-      });
-  
-      localStorage.setItem('collection', JSON.stringify(collection));
-    }
+
+    return collection;
   }
+
+  static newBook(book) {
+    const collection = Storage.getBooks();
+    collection.push(book);
+    localStorage.setItem('collection', JSON.stringify(collection));
+  }
+
+  static delBook(author) {
+    const collection = Storage.getBooks();
+
+    collection.forEach((book, index) => {
+      if (book.author === author) {
+        collection.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('collection', JSON.stringify(collection));
+  }
+}
